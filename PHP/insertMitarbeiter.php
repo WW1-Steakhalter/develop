6415febcc9b2e9242c17cf89ea91feed171eb026
nkunz <nkunze923@gmail.com>
2025-05-27 10:57:53 +0200

    added charset for db

--- PHP/insertMitarbeiter.php
+++ PHP/insertMitarbeiter.php
@@ -53,6 +53,8 @@ if ($conn->connect_error) {
     die(" Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
 
+$conn->set_charset("utf8mb4");
+
 // SQL vorbereiten
 $sql = "INSERT INTO mitarbeiter (
     name, mitarbeiter_id, entgeltgruppe,
